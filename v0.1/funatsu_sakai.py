4d803881ad49d9e56f6c43d7f18aceb22e296f71
Frank <induction04@naver.com>
2026-05-24 10:54:44 +0900

    Auto-sync with GSheets & Minor updates (2026-05-24)

--- v0.1/funatsu_sakai.py
+++ v0.1/funatsu_sakai.py
@@ -21,9 +21,9 @@ def render_sakai_analysis(df_raw):
     
     final_df = pd.DataFrame(analysis_data)
     st.markdown(final_df.to_html(escape=False, index=False), unsafe_allow_html=True)
-'''    
+
 # 2. Streamlit 자체 dataframe 기능을 사용해 표 렌더링
-    st.data_editor(
+    st.dataframe(
         final_df,
         use_container_width=True,  # 매끄럽게 화면 꽉 채우기
         hide_index=True,           # 왼쪽 인덱스 번호 숨기기
@@ -36,9 +36,8 @@ def render_sakai_analysis(df_raw):
                 "선별된 번호",
                 width="large"
             )
-        },
-        disabled=True
-    )'''
+        }
+    )
     
 def make_funatsu_sakai_pool(last_winning_numbers, last_bonus_number):
     
